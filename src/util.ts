aa5a35fdeacb1ccf8e57a493aa3c0aed56f16f3e
Toby Devlin <toby@tobydevlin.com>
2024-01-03 18:27:52 +1100

    allow plaiceholder image fails

--- src/util.ts
+++ src/util.ts
@@ -3,21 +3,26 @@ import { getPlaiceholder, GetPlaiceholderReturn } from "plaiceholder";
 
 export async function getImagePlaiceholder(
   imagePath: string,
-): Promise<GetPlaiceholderReturn> {
-  if (imagePath.startsWith("http")) {
-    // web based
-    const buffer = await fetch(imagePath).then(async (res) =>
-      Buffer.from(await res.arrayBuffer()),
-    );
-    return await getPlaiceholder(buffer);
-  } else {
-    //  file based
-    let initialPath = "public/";
-    if (imagePath.startsWith("/")) {
-      // ensure we don't get a public//media situation
-      initialPath = "public";
+): Promise<GetPlaiceholderReturn | undefined> {
+  try {
+    if (imagePath.startsWith("http")) {
+      // web based
+      const buffer = await fetch(imagePath).then(async (res) =>
+        Buffer.from(await res.arrayBuffer()),
+      );
+      return await getPlaiceholder(buffer);
+    } else {
+      //  file based
+      let initialPath = "public/";
+      if (imagePath.startsWith("/")) {
+        // ensure we don't get a public//media situation
+        initialPath = "public";
+      }
+      const buffer = await fs.readFile(`${initialPath}${imagePath}`);
+      return await getPlaiceholder(buffer);
     }
-    const buffer = await fs.readFile(`${initialPath}${imagePath}`);
-    return await getPlaiceholder(buffer);
+  } catch (e) {
+    console.log(`WARNING! returning undefined for image at ${imagePath}`);
+    return undefined;
   }
 }
