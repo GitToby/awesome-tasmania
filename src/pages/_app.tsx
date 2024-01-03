438cb4e26b67f719c58efcc60fde6e9ac783739b
Toby Devlin <toby@tobydevlin.com>
2024-01-04 10:46:42 +1100

    update default OpenGraph image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
 import { AwaitHydration } from "@/components/AwaitHydration";
 import "@/styles/globals.css";
 import { DefaultSeo } from "next-seo";
-import type { AppProps } from "next/app";
-// pick from https://fonts.google.com/
+import type { AppProps } from "next/app"; // pick from https://fonts.google.com/
 import { Ruda as Font } from "next/font/google";
 import { useRouter } from "next/router";
 
@@ -21,6 +20,14 @@ export default function App({ Component, pageProps }: AppProps) {
         titleTemplate="%s | Awesome Tasmania"
         description="Embrace Awesome Tasmania, an island where nature unfolds its grandeur and spirited adventure beckons around every bend. From breathtaking landscapes to thrilling escapades, immerse yourself in a world where awe meets discovery. Experience the extraordinary on this Australian gem that promises endless wonder at every turn."
         canonical={url}
+        openGraph={{
+          images: [
+            {
+              url: "https://awesome-tasmania.netlify.app/media/images/tasmania-from-space.jpg",
+              alt: "Tasmania from space",
+            },
+          ],
+        }}
       />
       <AwaitHydration>
         <Component {...pageProps} />
