b93586cc5b6d95cb549fc79e262992941dfb5572
Toby Devlin <toby@tobydevlin.com>
2024-01-03 18:22:49 +1100

    allow http images for plaiceholder

--- src/util.ts
+++ src/util.ts
@@ -4,11 +4,20 @@ import { getPlaiceholder, GetPlaiceholderReturn } from "plaiceholder";
 export async function getImagePlaiceholder(
   imagePath: string,
 ): Promise<GetPlaiceholderReturn> {
-  let initialPath = "public/";
-  if (imagePath.startsWith("/")) {
-    // ensure we don't get a public//media situation
-    initialPath = "public";
+  if (imagePath.startsWith("http")) {
+    // web based
+    const buffer = await fetch(imagePath).then(async (res) =>
+      Buffer.from(await res.arrayBuffer()),
+    );
+    return await getPlaiceholder(buffer);
+  } else {
+    //  file based
+    let initialPath = "public/";
+    if (imagePath.startsWith("/")) {
+      // ensure we don't get a public//media situation
+      initialPath = "public";
+    }
+    const buffer = await fs.readFile(`${initialPath}${imagePath}`);
+    return await getPlaiceholder(buffer);
   }
-  const file = await fs.readFile(`${initialPath}${imagePath}`);
-  return await getPlaiceholder(file);
 }
